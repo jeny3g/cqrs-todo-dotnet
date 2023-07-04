69f86a5418f25f7113cb609856c1de38bb13a396
Jean Oliveira Miranda <jean.miranda@iteris.com.br>
2023-07-04 12:00:49 -0300

    ListTodoItemQueryHandlerTest

--- tests/TODO.Service.UnitTest/Application/TodoItems/Queries/List/ListTodoItemQueryHandlerTest.cs
+++ tests/TODO.Service.UnitTest/Application/TodoItems/Queries/List/ListTodoItemQueryHandlerTest.cs
@@ -9,31 +9,42 @@ public class ListTodoItemQueryHandlerTest
     private ITodoContext _context;
     private ListTodoItemQueryHandler _sut;
 
+    private Faker _faker;
+
     private ListTodoItemQueryBuilder _listTodoItemQueryBuilder;
     private TodoItemBuilder _todoItemBuilder;
 
     public ListTodoItemQueryHandlerTest()
     {
-        SetupDependencies();
+        Setup();
+        InitializeBuilders();
         InitializeTestData();
+        InitializeDependencies();
     }
 
-    private void SetupDependencies()
+    private void Setup()
     {
+        _faker = new Faker();
         _context = Substitute.For<ITodoContext>();
         _sut = new ListTodoItemQueryHandler(_context);
     }
 
-    private void InitializeTestData()
+    private void InitializeBuilders()
     {
         _listTodoItemQueryBuilder = ListTodoItemQueryBuilder.New();
         _todoItemBuilder = TodoItemBuilder.New();
-
-        var set = new List<TodoItem>() { _todoItemBuilder.Build() }.AsDbSet();
-
-        _context.TodoItems.Returns(set);
     }
 
+    private void InitializeTestData()
+    {
+        // if there's data that needs to be initialized, add it here
+    }
+
+    private void InitializeDependencies()
+    {
+        var setTodoItems = new List<TodoItem>().AsDbSet();
+        _context.TodoItems.Returns(setTodoItems);
+    }
 
     [Fact]
     public async Task Handle_ShouldReturnActiveTodoItems()
@@ -41,13 +52,7 @@ public class ListTodoItemQueryHandlerTest
         var activeTodoItem = _todoItemBuilder.WithActive(true).Build();
         var inactiveTodoItem = _todoItemBuilder.WithActive(false).Build();
 
-        var setTodoItems = new List<TodoItem>
-        {
-            activeTodoItem,
-            inactiveTodoItem
-        }.AsDbSet();
-
-        _context.TodoItems.Returns(setTodoItems);
+        SetupTodoItems(new List<TodoItem> { activeTodoItem, inactiveTodoItem });
 
         var request = _listTodoItemQueryBuilder.Build();
 
@@ -56,5 +61,11 @@ public class ListTodoItemQueryHandlerTest
         Assert.Single(result);
         Assert.Equal(activeTodoItem.Id, result[0].Id);
         Assert.True(result[0].Active);
+    }
+
+    private void SetupTodoItems(List<TodoItem> todoItems)
+    {
+        var setTodoItems = todoItems.AsDbSet();
+        _context.TodoItems.Returns(setTodoItems);
     }
 }
